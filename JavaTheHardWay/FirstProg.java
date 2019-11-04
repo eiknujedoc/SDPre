af358440fb9fcb31b5a0abbab0d53f507f70302f
eiknujedoc <code.junkie@comcast.net>
2019-11-04 14:19:24 -0700

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Mark(TheWizard), am determined to learn how to code.");
+    System.out.println( "Today's date is November 4, 2019" );
+
+  }
+}
