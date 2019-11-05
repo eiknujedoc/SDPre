9518b3444d26c63257d5656eb1eb1ed3d0b0bb2c
eiknujedoc <code.junkie@comcast.net>
2019-11-04 17:42:46 -0700

    finish obj2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Blood";
+    food2 = "Guts";
+    food3 = "Glory";
+
+    System.out.println( "My Favorite Foods Are: " + food1 +", " + food2 + ", and " + food3 + "!");
+  }
+}
