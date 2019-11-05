e67d5723aaa967cdcdde62eda06c56ea70fd7da6
eiknujedoc <code.junkie@comcast.net>
2019-11-04 17:36:32 -0700

    finish Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Mark Walker Jr.";
+    int age = 37;
+    String state = "Washington";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
